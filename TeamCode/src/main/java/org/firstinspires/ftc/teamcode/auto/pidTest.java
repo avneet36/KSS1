83fd34bee809ae7a815778b40c9b42f28cf05819
KSSONE <kssrobotics9@gmail.com>
2023-02-17 16:49:37 -0800

    Update pidTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pidTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pidTest.java
@@ -19,11 +19,6 @@ public class pidTest extends LinearOpMode {
     public static double Ki = 0;
     public static double Kd = 0;
 
-    private DcMotorEx motorBackLeft;
-    private DcMotorEx motorBackRight;
-    private DcMotorEx motorFrontRight;
-    private DcMotorEx motorFrontLeft;
-
     FtcDashboard dashboard;
     ElapsedTime timer = new ElapsedTime();
     private double lastError = 0;
@@ -31,10 +26,10 @@ public class pidTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
-        motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
-        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
-        motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
+        DcMotorEx motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        DcMotorEx motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        DcMotorEx motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        DcMotorEx motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
 
         motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -74,7 +69,6 @@ public class pidTest extends LinearOpMode {
 
         timer.reset();
 
-        double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki);
-        return output;
+        return (error * Kp) + (derivative * Kd) + (integralSum * Ki);
     }
 }
