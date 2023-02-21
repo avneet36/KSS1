8654c7075e6d218f5f962199a2f51145008c4226
KSSONE <kssrobotics9@gmail.com>
2023-02-20 19:08:53 -0800

    More PID Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pidTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pidTest.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.auto;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -11,23 +10,23 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 @Autonomous(name = "PID Test")
 @Config
 public class pidTest extends LinearOpMode {
     double integralSum = 0;
-    public static double Kp = 0.002;
-    public static double Ki = 0.000005;
-    public static double Kd = 0.00005;
-    private DcMotorEx motorFrontLeft = null;
-    private DcMotorEx motorBackLeft = null;
-    private DcMotorEx motorFrontRight = null;
-    private DcMotorEx motorBackRight = null;
+    public static double Kp = 0.04;
+    public static double Ki = 0.000001;
+    public static double Kd = 0.0004;
+
+    private DcMotorEx motorBackLeft;
+    private DcMotorEx motorBackRight;
+    private DcMotorEx motorFrontRight;
+    private DcMotorEx motorFrontLeft;
 
     FtcDashboard dashboard;
     ElapsedTime timer = new ElapsedTime();
     private double lastError = 0;
-    public static double targetPosition;
+    public static double targetPosition = 1000;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -35,6 +34,10 @@ public class pidTest extends LinearOpMode {
         motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
         motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
         motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
+        DcMotorEx motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        DcMotorEx motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        DcMotorEx motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        DcMotorEx motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
 
         motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -47,43 +50,30 @@ public class pidTest extends LinearOpMode {
         motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
-        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-
-
-        dashboard = FtcDashboard.getInstance();
-        Telemetry dashboardTelemetry = dashboard.getTelemetry();
-
-        waitForStart();
-        while (opModeIsActive()) {
-            moveForward(1000);
-            dashboardTelemetry.addData("Motor Position ", motorFrontRight.getCurrentPosition());
-            dashboardTelemetry.addData("Target Position", targetPosition);
-            telemetry.addData("Power", motorFrontLeft.getPower());
-            dashboardTelemetry.update();
-            }
-        }
-
-    private void moveForward(int targetPosition){
 
         motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
         motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        dashboard = FtcDashboard.getInstance();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+        waitForStart();
+        while (opModeIsActive()) {
+            double power = PIDControl(targetPosition, motorFrontRight.getCurrentPosition());
 
-        double power = PIDControl(targetPosition, motorFrontRight.getCurrentPosition());
+            dashboardTelemetry.addData("Motor Position ", motorFrontRight.getCurrentPosition());
+            dashboardTelemetry.addData("Power", power);
+            dashboardTelemetry.addData("Target Position", targetPosition);
+            dashboardTelemetry.addData("Timer", timer);
+            dashboardTelemetry.update();
 
-        motorFrontRight.setPower(power);
-        motorFrontLeft.setPower(power);
-        motorBackRight.setPower(power);
-        motorBackLeft.setPower(power);
+            motorFrontRight.setPower(power);
+            motorFrontLeft.setPower(power);
+            motorBackRight.setPower(power);
+            motorBackLeft.setPower(power);
+        }
     }
-
-    public double PIDControl(double reference, double state) {
+    private double PIDControl(double reference, double state) {
         double error = reference - state;
         integralSum += error * timer.seconds();
         double derivative = (error - lastError) / timer.seconds();
@@ -91,7 +81,6 @@ public class pidTest extends LinearOpMode {
 
         timer.reset();
 
-        //return (error * Kp) + (derivative * Kd) + (integralSum * Ki);
-        return Range.clip((error * Kp) + (derivative * Kd) + (integralSum * Ki), -0.5, 0.5 );
+        return Range.clip((error * Kp) + (derivative * Kd) + (integralSum * Ki),-0.4,0.4);
     }
 }
